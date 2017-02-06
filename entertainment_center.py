cb1dce45beab9c25237f042a8104a8fc151143c1
Jeyabalaji Chandrasekaran <jchan@Jeyabalajis-MacBook-Pro.local>
2017-02-06 13:53:23 -0800

    bad api response addition

--- entertainment_center.py
+++ entertainment_center.py
@@ -10,20 +10,25 @@ API_KEY = "2178202FC1DB6B1A582DF6C7FA1E4CFE"
 requested_movie_year = str(2014)
 URL = "http://api.cinemalytics.in/v2/movie/year/" + requested_movie_year + "/?auth_token=" + API_KEY
 
+check_for_site_availability = requests.get(URL)
+site_status_code = check_for_site_availability.status_code
+if(site_status_code == 200):
+	response_json = requests.get(URL).json() #GET MOVIE LIST TO JSON
 
-response_json = requests.get(URL).json() #GET MOVIE LIST TO JSON
 
-i = 0
-objects_from_listings = []
+	i = 0
+	objects_from_listings = []
 
-# GOING THROUGH ALL THE JSON DATA AND STORING ACCORDINGLY TO EACH OBJECT IN AN OBJECT ARRAY
-for i in range(0,len(response_json)):
-	objects_from_listings.append(media.Movie(response_json[i]['Title'],
-												response_json[i]['Description'],
-												response_json[i]['PosterPath'],
-												response_json[i]['TrailerLink']))
-	i = i + 1
+	# GOING THROUGH ALL THE JSON DATA AND STORING ACCORDINGLY TO EACH OBJECT IN AN OBJECT ARRAY
+	for i in range(0,len(response_json)):
+		objects_from_listings.append(media.Movie(response_json[i]['Title'],
+													response_json[i]['Description'],
+													response_json[i]['PosterPath'],
+													response_json[i]['TrailerLink']))
+		i = i + 1
 
-# COPYING TO NEW OBJECT ARRAY
-movies = objects_from_listings
-fresh_tomatoes.open_movies_page(movies)
+	# COPYING TO NEW OBJECT ARRAY
+	movies = objects_from_listings
+	fresh_tomatoes.open_movies_page(movies)
+else:
+	print("Connection Error. Check the API Key")
--- readme.txt
+++ readme.txt
@@ -1,10 +1,5 @@
 Fresh Tomatoes is an application that list all the Bollywood movies that was released in particular year set by the code. This application is build with the help of Cinemalytics API Trial version, which can be found at https://developers.cinemalytics.com . The API key valid only for 3 days after the generation. The present API key is valid until Feb 9,2017 09:00 HRS PST.
 
-Before starting the site, please make sure the API Key is valid. If has expired, please contact me to get a new API key. After getting the new API Key, please paste it as the value for the variable "API_KEY" present in the python file "entertainment_center.py".
-For Example,
-	
-	API_KEY = "2178202FC1DB6B1A1N4IIF87FA1E4CFE"
-
 You can even alter the movie list's year by changing the value of the variable "requested_movie_year" to a year that you want. For example 
 
 	requested_movie_year = str(2012)
@@ -12,3 +7,8 @@ You can even alter the movie list's year by changing the value of the variable "
 To Start the site, just go a terminal and type
 	
 	python entertainment_center.py
+
+If the code throws your a Message "Connection Error. Check the API Key", please contact me to get a new API key. After getting the new API Key, please paste it as the value for the variable "API_KEY" present in the python file "entertainment_center.py".
+For Example,
+	
+	API_KEY = "2178202FC1DB6B1A1N4IIF87FA1E4CFE"
